7306e4af2763f332a02cf4a7d075ee5c37eac246
仲優樹 <yu-ki216m@outlook.jp>
2025-06-10 14:50:39 +0900

    Created AddButton.

---
+++ app/src/main/java/com/example/reusablebuttonsofjetpackcompose/AddButton.kt
@@ -0,0 +1,34 @@
+package com.example.reusablebuttonsofjetpackcompose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.reusablebuttonsofjetpackcompose.ui.theme.ReusableButtonsOfJetpackComposeTheme
+
+@Composable
+fun AddButton() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "追加",
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = 16.dp, end = 16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AddButtonPreview() {
+    ReusableButtonsOfJetpackComposeTheme {
+        AddButton()
+    }
+}
